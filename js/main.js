92912aa16a12c4176e73fcad2a4ee61056f0cbe8
Oleg <olberd@bk.ru>
2025-05-10 14:02:08 +0300

    add mobile menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 
 const navbar = document.querySelector(".navbar")
 const logo = document.querySelector(".logo-svg use")
+const mMenuToggle = document.querySelector(".mobile-menu-toggle")
+const menu = document.querySelector(".mobile-menu")
+
 window.addEventListener("scroll", () => {
     if (this.scrollY > 1) {
         navbar.classList.add("navbar-light");
@@ -10,4 +13,9 @@ window.addEventListener("scroll", () => {
         navbar.classList.remove("navbar-light");
         logo.href.baseVal = "img/sprite.svg#logo-light";
     }
-})
+});
+
+mMenuToggle.addEventListener("click", (event) => {
+    event.preventDefault();
+    menu.classList.toggle("is-open");
+});
